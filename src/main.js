bae2b35667f41fe6b70f525d19ed2f5666b8f134
Abhimanyu Aryan <abhimanyu.chat@gmail.com>
2018-08-20 18:46:50 +0530

    Completing Transaction code

---
+++ src/main.js
@@ -0,0 +1,121 @@
+const SHA256 = require("crypto-js/sha256");
+
+class Transaction{
+    constructor(fromAddress, toAddress, amount){
+        this.fromAddress = fromAddress;
+        this.toAddress = toAddress;
+        this.amount = amount;
+    }
+}
+
+class Block {
+    constructor(timestamp, transactions, previousHash = '') {
+        this.previousHash = previousHash;
+        this.timestamp = timestamp;
+        this.transactions = transactions;
+        this.hash = this.calculateHash();
+        this.nonce = 0;
+    }
+
+    calculateHash() {
+        return SHA256(this.previousHash + this.timestamp + JSON.stringify(this.transactions) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty) {
+        while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+
+        console.log("BLOCK MINED: " + this.hash);
+    }
+}
+
+
+class Blockchain{
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+        this.difficulty = 2;
+        this.pendingTransactions = [];
+        this.miningReward = 100;
+    }
+
+    createGenesisBlock() {
+        return new Block(Date.parse("2017-01-01"), [], "0");
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    minePendingTransactions(miningRewardAddress){
+        const rewardTx = new Transaction(null, miningRewardAddress, this.miningReward);
+        this.pendingTransactions.push(rewardTx);
+
+        let block = new Block(Date.now(), this.pendingTransactions, this.getLatestBlock().hash);
+        block.mineBlock(this.difficulty);
+
+        console.log('Block successfully mined!');
+        this.chain.push(block);
+
+        this.pendingTransactions = [];
+    }
+
+    createTransaction(transaction){
+        this.pendingTransactions.push(transaction);
+    }
+
+    getBalanceOfAddress(address){
+        let balance = 0;
+
+        for(const block of this.chain){
+            for(const trans of block.transactions){
+                if(trans.fromAddress === address){
+                    balance -= trans.amount;
+                }
+
+                if(trans.toAddress === address){
+                    balance += trans.amount;
+                }
+            }
+        }
+
+        return balance;
+    }
+
+    isChainValid() {
+        for (let i = 1; i < this.chain.length; i++){
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if (currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            }
+
+            if (currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+let arCoin = new Blockchain();
+arCoin.createTransaction(new Transaction('address1', 'address2', 100));
+arCoin.createTransaction(new Transaction('address2', 'address1', 50));
+
+console.log('\n Starting the miner...');
+arCoin.minePendingTransactions('abhimanyuaryan-address');
+
+console.log('\n Balance of Abhimanyu Aryan is', arCoin.getBalanceOfAddress('abhimanyuaryan-address'));
+
+// console.log('Mining block 1....');
+// arCoin.addBlock(new Block(1, "10/07/2017", { amount: 4}));
+// console.log('Mining block 2....');
+// arCoin.addBlock(new Block(2, "12/07/2017", { amount: 10}));
+
+//console.log('Is blockchain valid? ' + arCoin.isChainValid());
+
+// console.log('Is blockchain valid? ' + arCoin.isChainValid());
+// console.log(JSON.stringify(arCoin, null, 4));
