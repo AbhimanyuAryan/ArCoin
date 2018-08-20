dc617d9757cfd76aac9646afd638a22793436adc
Abhimanyu Aryan <abhimanyu.chat@gmail.com>
2018-08-20 16:31:38 +0530

    Mining Block code

--- main.js
+++ main.js
@@ -7,15 +7,26 @@ class Block{
         this.data = data;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
+        this.nonce = 0;
     }
+
     calculateHash(){
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty) {
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+        console.log("Block mined: " + this.hash);
     }
 }
 
 class Blockchain{
     constructor() {
         this.chain = [this.createGenesisBlock()];
+        this.difficulty = 2;
     }
 
     createGenesisBlock() {
@@ -28,7 +39,7 @@ class Blockchain{
 
     addBlock(newBlock) {
         newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
+        newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
     }
 
@@ -51,7 +62,10 @@ class Blockchain{
 }
 
 let arcoin = new Blockchain();
+
+console.log('Mining block 1....');
 arcoin.addBlock(new Block(1, "10/07/2017", { amount: 4}));
+console.log('Mining block 2....');
 arcoin.addBlock(new Block(2, "12/07/2017", { amount: 10}));
 
 console.log('Is blockchain valid? ' + arcoin.isChainValid());
